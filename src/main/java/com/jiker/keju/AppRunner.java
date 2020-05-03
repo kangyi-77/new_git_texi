fd75cfe160cc82b8ff9139a2eec52c0be49f3a9a
root <kangyi@shgbit.com>
2020-05-03 09:51:17 +0800

    commit

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -1,4 +1,5 @@
 package com.jiker.keju;
+
 import com.jiker.keju.taxi.CalculateFare;
 
 import java.io.BufferedReader;
@@ -9,35 +10,36 @@ import static java.util.regex.Pattern.compile;
 
 public class AppRunner {
 
-        public static void main(String[] args) throws Exception {
-            String testDataFile =  args[0];;
-            String receipt= new AppRunner().readFile(new File("src/main/resources/"+testDataFile));
-            System.out.println(receipt);
-        }
-
-        public String readFile(File file) throws Exception {
-            BufferedReader br = new BufferedReader(new java.io.FileReader(file));
-            StringBuffer sb = new StringBuffer();
-            String s;
-            while ((s = br.readLine()) != null) {
-                sb.append(getReceipt(getTimeAndDistance(s)));
-            }
-            br.close();
-            return sb.toString();
-        }
-
-        public int[] getTimeAndDistance(String str) {
-            String[] oneline = str.split(",");
-            Pattern p = compile("[^\\d]");
-            int[] result = new int[2];
-            result[0] = Integer.parseInt(p.matcher(oneline[0]).replaceAll(""));
-            result[1] = Integer.parseInt(p.matcher(oneline[1]).replaceAll(""));
-            return result;
-        }
-
-        public String getReceipt(int[] strline) {
-            int fare = new CalculateFare().calculate(strline[0], strline[1]);
-            String result = "收费" + fare + "元\n";
-            return result;
-        }
+    public static void main(String[] args) throws Exception {
+        String testDataFile = args[0];
+        ;
+        String receipt = new AppRunner().readFile(new File("src/main/resources/" + testDataFile));
+        System.out.println(receipt);
     }
+
+    public String readFile(File file) throws Exception {
+        BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+        StringBuffer sb = new StringBuffer();
+        String s;
+        while ((s = br.readLine()) != null) {
+            sb.append(getReceipt(getTimeAndDistance(s)));
+        }
+        br.close();
+        return sb.toString();
+    }
+
+    public int[] getTimeAndDistance(String str) {
+        String[] oneline = str.split(",");
+        Pattern p = compile("[^\\d]");
+        int[] result = new int[2];
+        result[0] = Integer.parseInt(p.matcher(oneline[0]).replaceAll(""));
+        result[1] = Integer.parseInt(p.matcher(oneline[1]).replaceAll(""));
+        return result;
+    }
+
+    public String getReceipt(int[] strline) {
+        int fare = new CalculateFare().calculate(strline[0], strline[1]);
+        String result = "收费" + fare + "元\n";
+        return result;
+    }
+}
--- src/main/java/com/jiker/keju/taxi/CalculateFare.java
+++ src/main/java/com/jiker/keju/taxi/CalculateFare.java
@@ -4,16 +4,12 @@ package com.jiker.keju.taxi;
 public class CalculateFare {
 
     public int calculate(int kilometre, int minute) {
-        float fare;
         if (kilometre <= 2) {
-            fare = Math.round(0.25 * minute + 6);
+            return (int) Math.round(0.25 * minute + 6);
         } else if (kilometre > 2 && kilometre <= 8) {
-            fare = Math.round((kilometre - 2) * 0.8 + 0.25 * minute + 6);
+            return (int) Math.round((kilometre - 2) * 0.8 + 0.25 * minute + 6);
         } else {
-            fare = Math.round(1.2 + 1.2 * kilometre + 0.25 * kilometre);
+            return (int) Math.round((kilometre - 2) * 0.8 + (kilometre - 8) * 0.5 + 0.25 * minute + 6);
         }
-        return (int) fare;
     }
-
-
 }

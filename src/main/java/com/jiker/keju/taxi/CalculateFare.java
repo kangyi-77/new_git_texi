6c09daf924579a657a8e21b8a45c0430117d2d7a
root <kangyi@shgbit.com>
2020-05-03 10:01:21 +0800

    commit

--- src/main/java/com/jiker/keju/taxi/CalculateFare.java
+++ src/main/java/com/jiker/keju/taxi/CalculateFare.java
@@ -2,14 +2,14 @@ package com.jiker.keju.taxi;
 
 
 public class CalculateFare {
-    
+
     public int calculate(int kilometre, int minute) {
         if (kilometre <= 2) {
             return (int) Math.round(0.25 * minute + 6);
         } else if (kilometre > 2 && kilometre <= 8) {
-            return (int) Math.round(4.4 + 0.8 * bean.dstance + 0.25 * bean.waiteTime);
+            return (int) Math.round(4.4 + 0.8 * kilometre + 0.25 * minute);
         } else {
-            return (int) Math.round(1.2 + 1.2 * bean.dstance + 0.25 * bean.waiteTime);
+            return (int) Math.round(1.2 + 1.2 * kilometre + 0.25 * minute);
         }
     }
 }

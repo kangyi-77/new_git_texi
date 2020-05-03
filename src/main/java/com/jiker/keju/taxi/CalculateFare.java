ff55419ec75527a5226b3391d5086a97b2ef5d23
root <kangyi@shgbit.com>
2020-05-03 12:06:41 +0800

    修改junit测试代码

--- src/main/java/com/jiker/keju/taxi/CalculateFare.java
+++ src/main/java/com/jiker/keju/taxi/CalculateFare.java
@@ -6,7 +6,7 @@ public class CalculateFare {
     public int calculate(int kilometre, int minute) {
         if (kilometre <= 2) {
             return (int) Math.round(0.25 * minute + 6);
-        } else if (kilometre > 2 && kilometre <= 8) {
+        } else if (kilometre <= 8) {
             return (int) Math.round(4.4 + 0.8 * kilometre + 0.25 * minute);
         } else {
             return (int) Math.round(1.2 + 1.2 * kilometre + 0.25 * minute);
--- src/test/java/com/jiker/keju/CalculateFareTest.java
+++ src/test/java/com/jiker/keju/CalculateFareTest.java
@@ -1,33 +1,32 @@
-package com.jiker.keju;
-
 import org.junit.Test;
+import com.jiker.keju.taxi.CalculateFare;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class CalculateFareTest {
+
     @Test
-    public void getReceipt_and_kilometre_2_test(){
-        int[] taxibo = new int[]{2,0};
-        String result = new AppRunner().getReceipt(taxibo);
-        assertEquals("收费6元\n",result);
+    public void getReceipt_and_kilometre_2_test() {
+        int result = new CalculateFare().calculate(2, 0);
+        assertEquals(6, result);
     }
+
     @Test
-    public void getReceipt_and_kilometre_6_test(){
-        int[] taxibo = new int[]{6,0};
-        String result = new AppRunner().getReceipt(taxibo);
-        assertEquals("收费9元\n",result);
+    public void getReceipt_and_kilometre_3_test() {
+        int result = new CalculateFare().calculate(3, 0);
+        assertEquals(7, result);
     }
+
     @Test
-    public void getReceipt_and_kilometre_10_test(){
-        int[] taxibo = new int[]{10,0};
-        String result = new AppRunner().getReceipt(taxibo);
-        assertEquals("收费13元\n",result);
+    public void getReceipt_and_kilometre_8_andminute_2_test() {
+        int result = new CalculateFare().calculate(8, 2);
+        assertEquals(11, result);
     }
+
     @Test
-    public void getReceipt_and_kilometre_10_and_minute_2_test(){
-        int[] taxibo = new int[]{10,2};
-        String result = new AppRunner().getReceipt(taxibo);
-        assertEquals("收费14元\n",result);
+    public void getReceipt_and_kilometre_10_and_minute_2_test() {
+        int result = new CalculateFare().calculate(10, 2);
+        assertEquals(14, result);
     }
 }
